020327a31040035a610ee91437acb035d230166e
wangyan3 <wangyan3@sensetime.com>
2022-04-11 17:12:28 +0800

    add EOC submodule

---
+++ EOC/prototype/data/datasets/__init__.py
@@ -0,0 +1,5 @@
+from .imagenet_dataset import ImageNetDataset, RankedImageNetDataset # noqa
+from .custom_dataset import CustomDataset # noqa
+
+from .imagnetc import ImageNet_C_Dataset
+
---
+++ EOC/prototype/model/layer/__init__.py
@@ -0,0 +1,5 @@
+from .condconv import CondConv2d  # noqa: F401
+from .splat import SplAtConv2d, rSoftMax  # noqa: F401
+from .weightnet import WeightNet, WeightNet_DW  # noqa: F401
+from .sparse_layer import SparseConv2d, SparseLinear  # noqa: F401
+from .drop_path import DropPath  # noqa: F401
